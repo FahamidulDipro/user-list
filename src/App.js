e32d65839ccefb427a47ba0c92cf71b40d51d59e
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-06-26 01:06:30 +0600

    404 page not found added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/signup" element={<Signup></Signup>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div style={{marginTop:"100px"}}>
+            <h1><span className='text-danger fw-bold'>404!</span> Page Not Found!</h1>
+        </div>
+    );
+};
+
+export default NotFound;
