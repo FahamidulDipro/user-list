c32f05ac6bfa846fdbf5da66f1fa68ecf75630d5
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-06-26 12:26:33 +0600

    using react query to fetch data from api

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 
 const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const {
+    data: users,
+    isLoading,
+    refetch,
+  } = useQuery("userData", () =>
+    fetch("https://randomuser.me/api/?results=500").then((res) => {
+      refetch();
+      return res.json();
+    })
+  );
+
+  return <div>{console.log(users)}</div>;
 };
 
 export default Home;
