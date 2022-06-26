dc4444dd0391260eb7accc09dc7b8cb92ccba874
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-06-26 12:54:09 +0600

    User data loaded in UI

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useQuery } from "react-query";
+import { ListGroup, Badge } from "react-bootstrap";
 
 const Home = () => {
   const {
@@ -13,7 +14,39 @@ const Home = () => {
     })
   );
 
-  return <div>{console.log(users)}</div>;
+  return (
+    <ListGroup as="ol" numbered>
+      {users?.results?.map((user) => (
+        <ListGroup.Item
+          as="li"
+          className="d-flex justify-content-between align-items-start"
+        >
+          <div className="ms-2 me-auto d-flex text-start">
+            <img
+              src={user.picture.thumbnail}
+              alt="userImg"
+              style={{ height: "70px", width: "70px", borderRadius: "50%" }}
+            />
+            <div className="fw-bold mx-3">
+              <span style={{ fontSize: "20px" }}>
+                {" "}
+                {user.name.first} {user.name.last}
+              </span>
+
+              <p>
+                Email:{" "}
+                <span className="fw-normal text-primary">{user.email}</span>
+              </p>
+              <p>
+                Phone:{" "}
+                <span className="fw-normal text-success">{user.phone}</span>
+              </p>
+            </div>
+          </div>
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  );
 };
 
 export default Home;
